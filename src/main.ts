4f2204baa7fb9f989a1b0fa5b147eca269014827
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2022-04-14 17:19:55 +0200

    - prisma init  - db init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,55 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express, { Application, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 // Boot express
 const app: Application = express()
-const port = 5000
+const prisma = new PrismaClient()
+const port = 3000
 
-// Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-	res.status(200).send({ data: 'Hello from Ornio AS' })
+app.use(express.json())
+
+
+app.post('/', async (req: Request, res: Response) => {
+	const {username, password} = req.body,
+		user = await prisma.user.create({
+			data: {
+				username: username,
+				password: password
+			}
+		})
+	
+	res.json(user)
+})
+
+app.get('/', async (req: Request, res: Response) => {
+	const users = await prisma.user.findMany()
+
+	res.json(users)
+})
+
+app.put('/', async (req: Request, res: Response) => {
+	const { id, username } = req.body,
+		updatedUser = await prisma.user.update({
+			where: {
+				id: id
+			},
+			data: {
+				username: username
+			}
+		})
+	
+	res.json(updatedUser)
+})
+
+app.delete('/:id', async (req: Request, res: Response) => {
+	const id = req.params.id,
+		deletedUser = await prisma.user.delete({
+			where: {
+				id: Number(id)
+			}
+		})
+
+	res.json(deletedUser)
 })
 
 // Start server
